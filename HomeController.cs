b73f06b24d9a301c51a2d01950e1132c05645e4f
bhavyashekar <bhavya.a.c@gmail.com>
2018-06-03 13:33:06 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,19 @@
+namespace VotingSystem.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+    }
+}
---
+++ VoteController.cs
@@ -0,0 +1,64 @@
+﻿namespace VotingSystem.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using VotingSystem.Data.Entity;
+    using VotingSystem.Data.Repositories;
+    using VotingSystem.Web.Common;
+
+    [Route("api")]
+    public class VoteController : Controller
+    {
+        private IVoteRepository _repository;
+        private readonly ILogger _logger;
+
+
+        public VoteController(IVoteRepository repository, ILogger<PersonaController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+
+        [Route("[controller]")]
+        [HttpPost]
+        public IActionResult Create([FromBody]Vote vote)
+        {
+            if (vote == null || string.IsNullOrEmpty(vote.contestantid)) { return BadRequest("Invalid Vote"); }
+
+            try
+            {
+                return Ok(_repository.Create(vote));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, ErrorMessages.InternalServerError);
+            }
+        }
+
+        [Route("[controller]")]
+        [HttpPost]
+        public IActionResult Create([FromBody]VoteAudit voteaudit)
+        {
+            if (voteaudit == null || string.IsNullOrEmpty(voteaudit.aadharnumber)) { return BadRequest("Invalid Voter"); }
+
+            try
+            {
+                return Ok(_repository.Create(voteaudit));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, ErrorMessages.InternalServerError);
+            }
+        }
+
+  
+    }
+}
